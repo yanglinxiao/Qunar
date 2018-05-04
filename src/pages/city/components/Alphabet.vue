497990a87ab3c1d1362a86956ad593070fd73050
yanglinxiao <511500064@qq.com>
2018-05-04 15:05:56 +0800

    city list alphabet finish

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="city-alphabet">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "CityAlphabet"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../../styles/variable.styl"
+  .city-alphabet
+    position absolute
+    top 1.6rem
+    right 0
+    bottom 0
+    display flex
+    flex-direction column
+    justify-content center
+    .item
+      line-height .4rem
+      color $themeBgColor
+      text-align center
+      padding-right .1rem
+</style>
