81bc65fd93107414af588a4473a5178e2f081351
yanglinxiao <511500064@qq.com>
2018-05-07 16:40:48 +0800

    detail-list finish

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,17 +2,43 @@
   <div class="detail">
     <detail-header></detail-header>
     <detail-banner></detail-banner>
+    <detail-list :list="list"></detail-list>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: "Detail",
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '体育中心店'
+            },
+            {
+              title: '北京路店'
+            }
+          ]
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '儿童票'
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="detail-list">
+    <div class="item border-bottom"
+        v-for="(item,index) of list"
+        :key="index">
+      <div class="item-header">
+        <span class="item-icon"></span>
+        <span class="item-title">{{item.title}}</span>
+      </div>
+      <div v-if="item.children" class="item-children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailList",
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-header
+    .item-icon
+      display inline-block
+      width .36rem
+      height .36rem
+      position relative
+      top .08rem
+      left .1rem
+      background url('http://s.qunarzz.com/piao/image/touch/sight/detail.png') 0 -.45rem no-repeat
+      margin-right .1rem
+      background-size .4rem 3rem
+    .item-title
+      height .6rem
+      line-height .6rem
+      font-size .3rem
+  .item-children
+    padding 0 .2rem
+</style>
